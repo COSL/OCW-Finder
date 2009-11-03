ca74d0136918a3e35c32c952fdfdd8534cccd42f
Justin Ball <justinball@gmail.com>
2009-11-02 21:15:16 -0700

    removing extra stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,6 @@ class ApplicationController < ActionController::Base
     
   protected
   
-  # called by Admin::Muck::BaseController to check whether or not the
-  # user should have access to the admin UI
-  def admin_access?
-    access_denied unless admin?
-  end
-  
   def setup_paging
     @page = (params[:page] || 1).to_i
     @page = 1 if @page < 1
--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -1,18 +1,5 @@
 class DefaultController < ApplicationController
 
-  def contact
-    return unless request.post?
-    body = []
-    params.each_pair do |k,v|
-      if !['authenticity_token', 'action', 'controller'].include?(k) 
-        body << "#{k}: #{v}"
-      end
-    end
-    BasicMailer.deliver_mail(:subject => I18n.t("contact.contact_response_subject", :application_name => GlobalConfig.application_name), :body=>body.join("\n"))
-    flash[:notice] = I18n.t('general.thank_you_contact')
-    redirect_to contact_url    
-  end
-
   def about
     respond_to do |format|
       format.html { render }
