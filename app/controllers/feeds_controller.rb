7c34e0e777c8a196e8f7661b346f404c55d8b797
Justin Ball <justinball@gmail.com>
2009-11-11 15:46:48 -0700

    added redirect for show

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -4,6 +4,10 @@ class FeedsController < ApplicationController
     permanent_redirect("http://www.folksemantic.com")
   end
   
+  def show
+    permanent_redirect("http://www.folksemantic.com")
+  end
+  
   def new
     permanent_redirect("http://www.folksemantic.com/feeds/new")
   end
