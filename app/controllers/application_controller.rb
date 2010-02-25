d6ab61ec827b704017ecbc373833dc071c904e99
Justin Ball <justinball@gmail.com>
2010-02-25 12:00:19 -0700

    added filters for locale and paging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,26 +4,30 @@ class ApplicationController < ActionController::Base
     
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-    
+  
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+  
+  before_filter :set_locale
+  before_filter :setup_paging
+  before_filter :set_will_paginate_string
+  
   protected
   
-  def permanent_redirect(url)
-    headers["Status"] = "301 Moved Permanently"
-    redirect_to url
-  end
+    def set_locale
+      discover_locale
+    end
+    
+    def permanent_redirect(url)
+      headers["Status"] = "301 Moved Permanently"
+      redirect_to url
+    end
   
-  def current_user
-    nil
-  end
+    def current_user
+      nil
+    end
   
-  def setup_paging
-    @page = (params[:page] || 1).to_i
-    @page = 1 if @page < 1
-    @per_page = (params[:per_page] || (Rails.env=='test' ? 1 : 40)).to_i
-  end
-  
-  def ssl_required?
-    false
-  end
+    def ssl_required?
+      false
+    end
   
 end
