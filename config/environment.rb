8a2272e1e7c20dcc14525ceba879744f59b6bbe6
Joel Duffin <Joel@Joel-Duffins-iMac.local>
2010-03-04 10:13:19 -0700

    Updated the muck services gem version

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   config.gem 'bcrypt-ruby', :lib => 'bcrypt', :version => '>=2.1.1'
   config.gem 'will_paginate'
   config.gem 'muck-engine', :lib => 'muck_engine', :version => '>=0.4.13'
-  config.gem 'muck-services', :lib => 'muck_services', :version => '>=0.1.38'
+  config.gem 'muck-services', :lib => 'muck_services', :version => '>=0.1.40'
   config.gem 'muck-raker', :lib => 'muck_raker', :version => '>=0.3.13'
   config.gem 'muck-solr', :lib => 'acts_as_solr', :version => '>=0.4.7'
 
